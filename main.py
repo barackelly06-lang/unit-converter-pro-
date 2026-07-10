ba506800547a80d34e50d56e92fd125a956e3cc0
barackelly06-lang <barackelly06@gmail.com>
2026-07-10 21:47:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
+ 
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.spinner import Spinner
 
-class UnitConverterApp(App):
-    def build(self):
-        self.layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
+class UnitConverter(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.padding = 20
+        self.spacing = 10
         
-        self.label = Label(text='Unit Converter Pro', font_size=24)
-        self.layout.add_widget(self.label)
+        self.add_widget(Label(text='Unit Converter Pro', font_size=24, size_hint_y=0.2))
         
-        self.input = TextInput(text='1', multiline=False, font_size=20)
-        self.layout.add_widget(self.input)
+        self.input = TextInput(text='1', multiline=False, font_size=20, size_hint_y=0.15)
+        self.add_widget(self.input)
         
-        self.spinner_from = Spinner(
-            text='Meters',
-            values=('Meters', 'Kilometers', 'Centimeters', 'Inches', 'Feet')
-        )
-        self.layout.add_widget(self.spinner_from)
-        
-        self.spinner_to = Spinner(
-            text='Centimeters', 
-            values=('Meters', 'Kilometers', 'Centimeters', 'Inches', 'Feet')
-        )
-        self.layout.add_widget(self
+        self.spinner = Spinner(
+            text='Meters
