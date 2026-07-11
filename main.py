4374a8604ac5b212d65653441ef4703432256d3e
barackelly06-lang <barackelly06@gmail.com>
2026-07-11 17:55:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.app import App 
+from kivy.uix.boxlayout import BoxLayout 
+from kivy.uix.button import Button 
+from kivy.uix.textinput import TextInput 
+from kivy.uix.spinner import Spinner 
+from kivy.uix.label import Label
 
-class ConverterApp(App):
-    def build(self):
-        layout = BoxLayout(orientation='vertical', padding=20)
+class ConverterApp(App): 
+    def build(self): 
+        layout = BoxLayout(orientation='vertical', padding=20, spacing=10) 
+        self.input = TextInput(hint_text='Weka namba hapa', multiline=False, font_size=32, input_filter='float') 
+        layout.add_widget(self.input) 
         
-        self.input = TextInput(hint_text='Weka namba hapa', font
+        self.spinner_from = Spinner(text='Meters', values=('Meters', 'Kilometers', 'Grams', 'Kilograms')) 
+        self.spinner_to = Spinner(text='Kilometers', values=('Meters', 'Kilometers', 'Grams', 'Kilograms')) 
+        
+        layout.add_widget(self.spinner_from) 
+        layout.add_widget(self.spinner_to) 
+        
+        btn = Button
